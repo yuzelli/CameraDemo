aa71fa74df744aa8e41a65510632c3c8ae5b86cf
yuzelli <zsl5155@qq.com>
2017-05-16 18:49:00 +0800

    finish

---
+++ app/src/main/java/com/example/yuzelli/camerademo/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.yuzelli.camerademo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
